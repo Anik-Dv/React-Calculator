8e4a4749698e598572aaccd5aa63a881540aa15a
Anik-Dv <freelanceranik50@gmail.com>
2022-10-08 20:28:45 +0600

    completed build a react calculator app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import './App.css';
 
 function App() {
@@ -5,42 +6,81 @@ function App() {
   const calculatDigits = () => {
     const digits = [];
     for (let i = 1; i < 10; i++) {
-      digits.push(<button key={i}>{i}</button>);
+      digits.push(<button onClick={()=> updateCalc(i.toString())} key={i}>{i}</button>);
     }
     return digits;
   }
+
+  // create a useState for showing resultInput
+  const [result, setResult] = useState("");
+  const [calc, setCalc] = useState("");
+  const [clear, setClear] = useState(null);
+
+
+  const secondarybtn = ['%','+','-','*','/','.'];
+  const updateCalc = (value)=> {
+    if(
+      secondarybtn.includes(value) && calc === "" ||
+      secondarybtn.includes(value) && secondarybtn.includes(calc.slice(-1))
+    )
+    {
+      return;
+    }
+    setCalc(calc + value);
+      if(!secondarybtn.includes(value)){
+        setResult(eval(calc + value).toString());
+      }
+  }
+
+  const handleDelete = ()=>{
+    if (calc === '') {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+
+  const handleClear = ()=>{
+    if(clear === ''){
+      return;
+    }
+    setCalc('');
+  };
+
+  const handleCalculate = ()=> {
+    setCalc(eval(calc).toString());
+  };
 
   return (
     <div className="App">
       <div className='calculator'>
 
         <div className='result-display'>
-          <span className='showInput'>0</span>
+          {result ? <span className='result'>{result}</span> : ""}
           <br/>
-          <span className='result'>(0)</span>
+           <span className='showInput'>{ calc || "0"}</span>
         </div>
 
-        <div className='secondary-btn'>
-          <button>%</button>
-          <button>+</button>
-          <button>-</button>
-          <button>x</button>
-          <button>÷</button>
-          <button>DEL</button>
-          <button>CE</button>
+        <div className='secondarybtn'>
+          <button onClick={()=>{updateCalc('%')}}>%</button>
+          <button onClick={()=>{updateCalc('+')}}>+</button>
+          <button onClick={()=>{updateCalc('-')}}>-</button>
+          <button onClick={()=>{updateCalc('*')}}>x</button>
+          <button onClick={()=>{updateCalc('/')}}>÷</button>
+          <button onClick={handleDelete}>DEL</button>
+          <button onClick={handleClear}>CE</button>
 
         </div>
 
         <div className='digits-num'>
           <div className='digits'>
-
-            {calculatDigits()}
-            <button>.</button>
-            <button>0</button>
-            <button style={{ backgroundColor: "#e90a72", borderRadius: "10px" }}>=</button>
+            { calculatDigits() }
+            <button onClick={()=> updateCalc('.')}>.</button>
+            <button onClick={()=> updateCalc('0')}>0</button>
+            <button style={{ backgroundColor: "#e90a72", borderRadius: "10px" }} onClick={handleCalculate}>=</button>
           </div>
-
         </div>
+
       </div>
     </div>
   );
